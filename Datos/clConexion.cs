dcd6a5aa840d6d3fad85114f1566160720aad170
JhonnAcunn <jaacuna86@misena.edu.co>
2022-04-22 22:18:33 -0500

    clConexion

---
+++ Datos/clConexion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appEdificacion.Datos
+{
+    public class clConexion
+    {
+
+        SqlConnection conexion;
+        public clConexion()
+        {
+            conexion = new SqlConnection("Data Source=.;Initial Catalog=constructores;Integrated Security=True");
+            conexion.Open();
+
+        }
+        public DataTable mtdDesconectado(string sql)//select
+        {
+            SqlDataAdapter adaptador = new SqlDataAdapter(sql, conexion);
+            DataTable tblDatos = new DataTable();
+            adaptador.Fill(tblDatos);
+            conexion.Close();
+            return tblDatos;
+        }
+        public int mtdConectado(string sql)
+        {
+            SqlCommand comando = new SqlCommand(sql, conexion);
+
+            int filasAfectadas = comando.ExecuteNonQuery();
+            conexion.Close();
+            return filasAfectadas;
+
+        }
+
+
+    }
+}
+}
