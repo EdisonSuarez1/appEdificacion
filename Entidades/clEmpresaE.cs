e0d7d05a3b61336c564afcbdcc76987516ad8273
EdisonSuarez1 <easuarez94@misena.edu.co>
2022-04-22 22:09:36 -0500

    Subida de Entidades

---
+++ Entidades/clEmpresaE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appEdificacion.Entidades
+{
+    public class clEmpresaE
+    {
+        public int idEmpresa { get; set; }
+        public string nombreempresa { get; set; }
+        public string direcciones { get; set; }
+        public string telefono { get; set; }
+        public string ciudad { get; set; }
+    }
+}
