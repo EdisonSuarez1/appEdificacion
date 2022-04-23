c688f6e8af88b6edb7b553289eec3341e2cfc3c2
jeison sandoval <jeissonsandoval1993@gmail.com>
2022-04-22 22:25:26 -0500

    anadirNombre

---
+++ Datos/clJJJJ.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appEdificacion.Datos
+{
+    public class clJJJJ
+    {
+        public string nombre { get; set; }
+    }
+}
